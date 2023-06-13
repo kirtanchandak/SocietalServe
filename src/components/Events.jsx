8869a4922b27c440fd4689725d382e519d648a52
kirtanchandak <themarketingkirtan@gmail.com>
2023-06-13 13:30:04 +0530

    protected route and logout

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { account } from "../utils/appwrite";
+import Login from "./Login";
 
 function Events() {
+  const navigate = useNavigate();
+  const [user, setuser] = useState();
+
+  useEffect(() => {
+    const getData = account.get();
+    getData.then(
+      function (response) {
+        console.log(response);
+        setuser(response);
+      },
+      function (err) {
+        console.log(err);
+      }
+    );
+  }, []);
+
+  const logoutUser = async () => {
+    try {
+      await account.deleteSession("current");
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <>
-      <div>
-        <h1>ok</h1>
-      </div>
+      {user ? (
+        <div className="text-center p-6">
+          <button className="bg-red-200" onClick={logoutUser}>
+            Logout
+          </button>
+          <h1 className="text-3xl font-[700]">Events</h1>
+        </div>
+      ) : (
+        <Login />
+      )}
     </>
   );
 }
