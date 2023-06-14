c9f18765618b4fdb6a0b08c0c4ab690e900653ae
kirtanchandak <themarketingkirtan@gmail.com>
2023-06-14 23:32:05 +0530

    data added

--- src/components/EventPage.jsx
+++ src/components/EventPage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "../App.css";
+import { data } from "../utils/data";
 
 const EventPage = () => {
   const [img, setImg] = useState("");
@@ -8,36 +9,6 @@ const EventPage = () => {
   const [date, setDate] = useState("");
 
   useEffect(() => {
-    const data = [
-      {
-        id: 1,
-        img: "https://earth5r.org/wp-content/uploads/2020/07/Clean-Up-Drive-Gandhinagar-Mumbai-India-Environmental-NGO-Earth5R.jpg",
-        title: "Garbage collection Drive",
-        date: "FriDay 9th June 2023 2:15",
-        des: "Come Join us for the most beaautiful organisational comittie event :)",
-      },
-      {
-        id: 2,
-        img: "https://www.bookeventz.com/blog/wp-content/uploads/2021/02/FI-1.jpg",
-        date: "FriDay 9th June 2023 2:15",
-        title: "Food Distribution Drive",
-        des: "Come Join us for the most beaautiful organisational comittie event :)",
-      },
-      {
-        id: 3,
-        img: "https://static.toiimg.com/thumb/msid-68667160,width-400,resizemode-4/68667160.jpg",
-        date: "FriDay 9th June 2023 2:15",
-        title: "Blood Donation Camp",
-        des: "Come Join us for the most beaautiful organisational comittie event :)",
-      },
-      {
-        id: 4,
-        img: "https://ratnanidhi.org/userfile/image/Webp%20images/001F.webp",
-        date: "FriDay 9th June 2023 2:15",
-        title: "Clothes Distribution Drive",
-        des: "Come Join us for the most beaautiful organisational comittie event :)",
-      },
-    ];
     var url = window.location.href;
     url = url.split("/").pop();
     console.log(url);
---
+++ src/utils/data.js
@@ -0,0 +1,30 @@
+export const data = [
+  {
+    id: 1,
+    img: "https://earth5r.org/wp-content/uploads/2020/07/Clean-Up-Drive-Gandhinagar-Mumbai-India-Environmental-NGO-Earth5R.jpg",
+    title: "Garbage collection Drive",
+    date: "FriDay 9th June 2023 2:15",
+    des: "Come Join us for the most beaautiful organisational comittie event :)",
+  },
+  {
+    id: 2,
+    img: "https://www.bookeventz.com/blog/wp-content/uploads/2021/02/FI-1.jpg",
+    date: "FriDay 9th June 2023 2:15",
+    title: "Food Distribution Drive",
+    des: "Come Join us for the most beaautiful organisational comittie event :)",
+  },
+  {
+    id: 3,
+    img: "https://static.toiimg.com/thumb/msid-68667160,width-400,resizemode-4/68667160.jpg",
+    date: "FriDay 9th June 2023 2:15",
+    title: "Blood Donation Camp",
+    des: "Come Join us for the most beaautiful organisational comittie event :)",
+  },
+  {
+    id: 4,
+    img: "https://ratnanidhi.org/userfile/image/Webp%20images/001F.webp",
+    date: "FriDay 9th June 2023 2:15",
+    title: "Clothes Distribution Drive",
+    des: "Come Join us for the most beaautiful organisational comittie event :)",
+  },
+];
